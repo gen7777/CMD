51a1834a436b08e5cdba72952f9d1cd5c3e70078
gen7777 <gen777@gmail.com>
2021-09-12 19:50:15 +0300

    Объявление переменных

---
+++ CleanShareFolder.cmd
@@ -0,0 +1,13 @@
+rem ===================================================================
+rem Объявляем переменные
+set workFolder=C:\workFolder
+set folderClean=F:\Exchange\!Общая_папка
+rem *****
+set folderOrdersClients=F:\Exchange\!Общая_папка\Заказы_клиентов
+set tempFolderOrdersClients=%workFolder%\Заказы_клиентов
+rem *****
+set folderDogovor=F:\Exchange\!Общая_папка\dogovor
+set tempFolderDogovor=%workFolder%\dogovor
+rem
+rem ****************************************************
+rem
